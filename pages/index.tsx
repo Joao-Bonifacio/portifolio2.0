772cf2e6fa0dcb5b7e6e8ccbe5eb6eb13f5644b1
João Bonifácio <joaojunior.b22@gmail.com>
2023-04-17 14:11:51 -0300

    header

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,9 @@
 //import Image from 'next/image' (<Image src='...' widith='...' height='...' alt='...' />)
+import Header from '../components/header'
 export default function Home() {
   return (
     <>
+    <Header></Header>
       <h1>Hello World!</h1>
     </>
   )
