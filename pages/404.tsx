e98c62dcc57972d7c4a19bf1aa15b20a7ed8db89
João Bonifácio <joaojunior.b22@gmail.com>
2023-04-17 14:00:22 -0300

    portifolio.init

---
+++ pages/404.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function NotFound(){
+    return(
+        <>
+        <h1>404 - Page not found</h1>
+        <Link href='/'>go back home</Link>
+        </>
+    )
+}
---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+//import Image from 'next/image' (<Image src='...' widith='...' height='...' alt='...' />)
+export default function Home() {
+  return (
+    <>
+      <h1>Hello World!</h1>
+    </>
+  )
+}
