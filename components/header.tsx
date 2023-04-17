b42a1241228f7b99caf899e9dd2715c8b7c3e27f
João Bonifácio <joaojunior.b22@gmail.com>
2023-04-17 17:16:12 -0300

    tailwind-init&header_css

--- components/header.tsx
+++ components/header.tsx
@@ -3,19 +3,18 @@ import Link from "next/link"
 export default function Header() {
     return (
         <>
-            <header>
-                <span>
-                    <h1>🔵</h1>
-                    <nav>
-                        <ul>
-                            <li><Link href="/">Home</Link></li>
-                            <li><Link href="/sobre">Sobre</Link></li>
-                            <li><Link href="/contato">Contato</Link></li>
-                        </ul>
-                    </nav>
-                </span>
-            </header>
-            <hr />
+        <header className="bg-gray-800 py-4">
+            <nav className="container mx-auto flex justify-between items-center px-6">
+                <div>
+                    <span className="text-gray-300">Portifólio</span>
+                </div>
+                <div>
+                    <a href="/" className="text-gray-300 hover:text-white mr-4">Home</a>  
+                    <a href="/sobre" className="text-gray-300 hover:text-white mr-4">Sobre</a>
+                    <a href="/contato" className="text-gray-300 hover:text-white">Contato</a>
+                </div>
+            </nav>
+        </header>
         </>
     )
 }
