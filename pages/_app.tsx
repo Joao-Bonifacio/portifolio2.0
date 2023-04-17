c6a848d20592b9d68589ffbf8b2c8c808f2fa0de
João Bonifácio <joaojunior.b22@gmail.com>
2023-04-17 15:00:20 -0300

    fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,16 @@
-//import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
-//import Header from "../components/header"
+import Header from '../components/header'
 import React from 'react'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
   <>
-    <Component {...pageProps} />
+  <Head>
+    <title>Portifolio</title>
+  </Head>
+  <Header></Header>
+  <Component {...pageProps} />
   </>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,8 @@
 //import Image from 'next/image' (<Image src='...' widith='...' height='...' alt='...' />)
-import Header from '../components/header'
+
 export default function Home() {
   return (
     <>
-    <Header></Header>
       <h1>Hello World!</h1>
     </>
   )
