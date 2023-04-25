b3376a79882ce7edfe20e647da381bf6640622af
João Bonifácio <joaojunior.b22@gmail.com>
2023-04-25 18:46:57 -0300

    projects-skeleton-finished

--- pages/projetos/index.tsx
+++ pages/projetos/index.tsx
@@ -1,47 +1,49 @@
+import Image from "next/image"
+
 export default function Projects(){
     return (
         <>
             <h1 className="text-center text-xl text-gray-300 bg-gray-600 p-2"> Projetos </h1>
             <div className="p-8 flex flex-wrap justify-center bg-gray-400">
                 <div className="flex flex-wrap mx-5 bg-gray-300 p-5 rounded-md mb-8">
-                    <div className="max-w-sm mr-2">image...</div>
-                    <p className="max-w-xl">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem nihil nisi nemo adipisci ipsam. Eaque iste asperiores repellat consequatur in nisi sunt, quae, ipsam doloribus ad nobis! Neque, adipisci! Cumque.</p>
+                <Image src="" width="100" height="100" alt="..."/>
+                    <p className="max-w-md">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem nihil nisi nemo adipisci ipsam. Eaque iste asperiores repellat consequatur in nisi sunt, quae, ipsam doloribus ad nobis! Neque, adipisci! Cumque.</p>
                 </div>
                 <div className="flex flex-wrap mx-5 bg-gray-300 p-5 rounded-md mb-8">
-                    <div className="max-w-sm mr-2">image...</div>
-                    <p className="max-w-xl">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem nihil nisi nemo adipisci ipsam. Eaque iste asperiores repellat consequatur in nisi sunt, quae, ipsam doloribus ad nobis! Neque, adipisci! Cumque.</p>
+                <Image src="" width="100" height="100" alt="..."/>
+                    <p className="max-w-md">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem nihil nisi nemo adipisci ipsam. Eaque iste asperiores repellat consequatur in nisi sunt, quae, ipsam doloribus ad nobis! Neque, adipisci! Cumque.</p>
                 </div>
                 <div className="flex flex-wrap mx-5 bg-gray-300 p-5 rounded-md mb-8">
-                    <div className="max-w-sm mr-2">image...</div>
-                    <p className="max-w-xl">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem nihil nisi nemo adipisci ipsam. Eaque iste asperiores repellat consequatur in nisi sunt, quae, ipsam doloribus ad nobis! Neque, adipisci! Cumque.</p>
+                <Image src="" width="100" height="100" alt="..."/>
+                    <p className="max-w-md">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem nihil nisi nemo adipisci ipsam. Eaque iste asperiores repellat consequatur in nisi sunt, quae, ipsam doloribus ad nobis! Neque, adipisci! Cumque.</p>
                 </div>
                 <div className="flex flex-wrap mx-5 bg-gray-300 p-5 rounded-md mb-8">
-                    <div className="max-w-sm mr-2">image...</div>
-                    <p className="max-w-xl">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem nihil nisi nemo adipisci ipsam. Eaque iste asperiores repellat consequatur in nisi sunt, quae, ipsam doloribus ad nobis! Neque, adipisci! Cumque.</p>
+                <Image src="" width="100" height="100" alt="..."/>
+                    <p className="max-w-md">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem nihil nisi nemo adipisci ipsam. Eaque iste asperiores repellat consequatur in nisi sunt, quae, ipsam doloribus ad nobis! Neque, adipisci! Cumque.</p>
                 </div>
                 <div className="flex flex-wrap mx-5 bg-gray-300 p-5 rounded-md mb-8">
-                    <div className="max-w-sm mr-2">image...</div>
-                    <p className="max-w-xl">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem nihil nisi nemo adipisci ipsam. Eaque iste asperiores repellat consequatur in nisi sunt, quae, ipsam doloribus ad nobis! Neque, adipisci! Cumque.</p>
+                <Image src="" width="100" height="100" alt="..."/>
+                    <p className="max-w-md">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem nihil nisi nemo adipisci ipsam. Eaque iste asperiores repellat consequatur in nisi sunt, quae, ipsam doloribus ad nobis! Neque, adipisci! Cumque.</p>
                 </div>
                 <div className="flex flex-wrap mx-5 bg-gray-300 p-5 rounded-md mb-8">
-                    <div className="max-w-sm mr-2">image...</div>
-                    <p className="max-w-xl">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem nihil nisi nemo adipisci ipsam. Eaque iste asperiores repellat consequatur in nisi sunt, quae, ipsam doloribus ad nobis! Neque, adipisci! Cumque.</p>
+                <Image src="" width="100" height="100" alt="..."/>
+                    <p className="max-w-md">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem nihil nisi nemo adipisci ipsam. Eaque iste asperiores repellat consequatur in nisi sunt, quae, ipsam doloribus ad nobis! Neque, adipisci! Cumque.</p>
                 </div>
                 <div className="flex flex-wrap mx-5 bg-gray-300 p-5 rounded-md mb-8">
-                    <div className="max-w-sm mr-2">image...</div>
-                    <p className="max-w-xl">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem nihil nisi nemo adipisci ipsam. Eaque iste asperiores repellat consequatur in nisi sunt, quae, ipsam doloribus ad nobis! Neque, adipisci! Cumque.</p>
+                <Image src="" width="100" height="100" alt="..."/>
+                    <p className="max-w-md">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem nihil nisi nemo adipisci ipsam. Eaque iste asperiores repellat consequatur in nisi sunt, quae, ipsam doloribus ad nobis! Neque, adipisci! Cumque.</p>
                 </div>
                 <div className="flex flex-wrap mx-5 bg-gray-300 p-5 rounded-md mb-8">
-                    <div className="max-w-sm mr-2">image...</div>
-                    <p className="max-w-xl">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem nihil nisi nemo adipisci ipsam. Eaque iste asperiores repellat consequatur in nisi sunt, quae, ipsam doloribus ad nobis! Neque, adipisci! Cumque.</p>
+                <Image src="" width="100" height="100" alt="..."/>
+                    <p className="max-w-md">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem nihil nisi nemo adipisci ipsam. Eaque iste asperiores repellat consequatur in nisi sunt, quae, ipsam doloribus ad nobis! Neque, adipisci! Cumque.</p>
                 </div>
                 <div className="flex flex-wrap mx-5 bg-gray-300 p-5 rounded-md mb-8">
-                    <div className="max-w-sm mr-2">image...</div>
-                    <p className="max-w-xl">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem nihil nisi nemo adipisci ipsam. Eaque iste asperiores repellat consequatur in nisi sunt, quae, ipsam doloribus ad nobis! Neque, adipisci! Cumque.</p>
+                <Image src="" width="100" height="100" alt="..."/>
+                    <p className="max-w-md">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem nihil nisi nemo adipisci ipsam. Eaque iste asperiores repellat consequatur in nisi sunt, quae, ipsam doloribus ad nobis! Neque, adipisci! Cumque.</p>
                 </div>
                 <div className="flex flex-wrap mx-5 bg-gray-300 p-5 rounded-md mb-8">
-                    <div className="max-w-sm mr-2">image...</div>
-                    <p className="max-w-xl">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem nihil nisi nemo adipisci ipsam. Eaque iste asperiores repellat consequatur in nisi sunt, quae, ipsam doloribus ad nobis! Neque, adipisci! Cumque.</p>
+                <Image src="" width="100" height="100" alt="..."/>
+                    <p className="max-w-md">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem nihil nisi nemo adipisci ipsam. Eaque iste asperiores repellat consequatur in nisi sunt, quae, ipsam doloribus ad nobis! Neque, adipisci! Cumque.</p>
                 </div>
             </div>
         </>
