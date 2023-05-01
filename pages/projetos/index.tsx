b13e35160d6d63a9d0513eb94908be2092c75d09
João Bonifácio <joaojunior.b22@gmail.com>
2023-05-01 14:05:05 -0300

    refactoring-projects(json)

--- pages/projetos/index.tsx
+++ pages/projetos/index.tsx
@@ -1,66 +1,20 @@
 import Image from "next/image"
+import projects from "../../projects.json"
 
 export default function Projects(){
     return (
         <>
             <h1 className="text-center text-xl text-gray-300 bg-gray-600 p-2"> Projetos </h1>
             <div className="p-8 flex flex-wrap justify-center bg-gray-400">
-                <div className="flex flex-wrap mx-5 bg-gray-300 rounded-md mb-8">
-                <Image src="/images/upStep/upStep-cover.jpg" width="160" height="110" alt="..." className="justify-center mr-2 rounded-l-md"/>
-                    <div className="max-w-md p-4">
-                        <h4 className="font-bold">upStep</h4>
-                        <p>Aperfeiçoamento da produtividade, com gráficos interativos e editáveis, com áreas de conhecimento e projeções, controle de tempo e anotações.</p>
+                {projects.map(e => (
+                    <div className="flex flex-wrap mx-5 bg-gray-300 rounded-md mb-8" key={e.id}>
+                    <Image src={e.image} width="160" height="110" alt="..." className="justify-center mr-2 rounded-l-md"/>
+                        <div className="max-w-md p-4">
+                            <h4 className="font-bold">{e.title}</h4>
+                            <p>{e.short_desc}</p>
+                        </div>
                     </div>
-                </div>
-                <div className="flex flex-wrap mx-5 bg-gray-300 rounded-md mb-8">
-                <Image src="/images/newsLetter/cover.jpg" width="160" height="100" alt="..." className="justify-center mr-2 rounded-l-md"/>
-                    <div className="max-w-md p-4">
-                        <h4 className="font-bold">newsletter</h4>
-                        <p>Site de notícias com integração com banco de dados mongoDB, busca de notícias e hank de mais visitadas.</p>
-                    </div>
-                </div>
-                <div className="flex flex-wrap mx-5 bg-gray-300 rounded-md mb-8">
-                <Image src="/images/tradeBallance/cover.jpg" width="160" height="100" alt="..." className="justify-center mr-2 rounded-l-md"/>
-                    <div className="max-w-md p-4">
-                        <h4 className="font-bold">trade ballance</h4>
-                        <p>Gráfico interativo de variação de patrimonio, com valores editáveis, tais valores foram salvos no navegador do cliente.</p>
-                    </div>
-                </div>
-                <div className="flex flex-wrap mx-5 bg-gray-300 rounded-md mb-8">
-                <Image src="/images/hangMan/cover.jpg" width="160" height="100" alt="..." className="justify-center mr-2 rounded-l-md"/>
-                    <div className="max-w-md p-4">
-                        <h4 className="font-bold">jogo da forca</h4>
-                        <p>Jogo da forca baseado em manipulação de strings com JavaScript. todas as partes do boneco foram construidas inteiramente com CSS.</p>
-                    </div>
-                </div>
-                <div className="flex flex-wrap mx-5 bg-gray-300 rounded-md mb-8">
-                <Image src="/images/movieDB/cover.jpg" width="160" height="110" alt="..." className="justify-center mr-2 rounded-l-md"/>
-                    <div className="max-w-md p-4">
-                        <h4 className="font-bold">movie hank</h4>
-                        <p>Hank de filmes/series mais assistidas. Dados fornecidos pela API the movie db, com slide de fotos e os títulos contendo nomes e sinopses.</p>
-                    </div>
-                </div>
-                <div className="flex flex-wrap mx-5 bg-gray-300 rounded-md mb-8">
-                <Image src="/images/jokenpo/cover.jpg" width="160" height="100" alt="..." className="justify-center mr-2 rounded-l-md"/>
-                    <div className="max-w-md p-4">
-                        <h4 className="font-bold">jokenpo</h4>
-                        <p>Jogo jokenpo, player vs máquina, com geração de jogada aleatória por parte da máquina do cliente.</p>
-                    </div>
-                </div>
-                <div className="flex flex-wrap mx-5 bg-gray-300 rounded-md mb-8">
-                <Image src="/images/pokedex/secondary.jpg" width="160" height="100" alt="..." className="justify-center mr-2 rounded-l-md"/>
-                    <div className="max-w-md p-4">
-                        <h4 className="font-bold">pokedex</h4>
-                        <p>Geração de pokemons aleatórios de acordo com o número de pokemons passados como argumento na página.</p>
-                    </div>
-                </div>
-                <div className="flex flex-wrap mx-5 bg-gray-300 rounded-md mb-8">
-                <Image src="/images/calcOB/cover.jpg.png" width="160" height="100" alt="..." className="justify-center mr-2 rounded-l-md"/>
-                    <div className="max-w-md p-4">
-                        <h4 className="font-bold">calculadora OB</h4>
-                        <p>Programa de linha de comando para projeções de resultados/rentabilidade em opções binárias de acordo com cada tipo de gerenciamento.</p>
-                    </div>
-                </div>
+                ))}
             </div>
         </>
     )
