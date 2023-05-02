a9436ca4b21c29c415b6de55a8635a57ed2d4b58
João Bonifácio <joaojunior.b22@gmail.com>
2023-05-02 06:31:07 -0300

    dynamic-route-finished

--- pages/projetos/[projectId].tsx
+++ pages/projetos/[projectId].tsx
@@ -1,4 +1,6 @@
 import { useRouter } from "next/router"
+import Image from "next/image"
+import Link from "next/link"
 import projects from "@data/projects.json"
 
 export default function Projects(){
@@ -6,10 +8,21 @@ export default function Projects(){
     const projectId = router.query.projectId
     const project = projects.find(projects => projects.id.toString() == projectId)
 
-    return(
-        <>
-            <h1>oi</h1>
-            <div></div>
-        </>
-    )
+    if (project) {
+        return(
+            <>
+                <h1 className="text-center text-xl text-gray-300 bg-gray-600 p-2"> {project?.title} </h1>
+                <div>
+                    <Image src={project.image} width="300" height="250" alt={project.title}/>
+                </div>
+            </>
+        )
+    }else{
+        return(
+            <>
+                <h1>Projeto não encontrado</h1>
+                <Link href="/projetos">Voltar</Link>
+            </>
+        )
+    }
 }
--- pages/projetos/index.tsx
+++ pages/projetos/index.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image"
+import Link from "next/link"
 import projects from "@data/projects.json"
 
 export default function Projects(){
@@ -7,13 +8,13 @@ export default function Projects(){
             <h1 className="text-center text-xl text-gray-300 bg-gray-600 p-2"> Projetos </h1>
             <div className="p-8 flex flex-wrap justify-center bg-gray-400">
                 {projects.map(e => (
-                    <div className="flex flex-wrap mx-5 bg-gray-300 rounded-md mb-8" key={e.id}>
+                    <Link href={"/projetos/"+e.id} className="flex flex-wrap mx-5 bg-gray-300 hover:bg-gray-400 rounded-md mb-8" key={e.id}>
                     <Image src={e.image} width="160" height="110" alt="..." className="justify-center mr-2 rounded-l-md"/>
                         <div className="max-w-md p-4">
                             <h4 className="font-bold">{e.title}</h4>
                             <p>{e.short_desc}</p>
                         </div>
-                    </div>
+                    </Link>
                 ))}
             </div>
         </>
