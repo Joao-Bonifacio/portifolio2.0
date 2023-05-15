aed3432ce066c584f277efdcfc21720478d7ac48
João Bonifácio <joaojunior.b22@gmail.com>
2023-05-15 16:11:21 -0300

    index-fixed

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ import Projects from "@components/projects"
 export default function Home() {
   return (
     <>
-      <div className="bg-gray-400 p-10 mb-5">
+      <div className="bg-gray-400 p-10">
         <div className="bg-gray-400 flex flex-wrap m-auto justify-center items-start w-full">
           
           <div className="rounded-xl bg-gray-300 max-w-sm min-w-sm flex flex-wrap mb-5 mx-auto mm:w-1/2 ll:w-1/2 xl:w-1/3">
