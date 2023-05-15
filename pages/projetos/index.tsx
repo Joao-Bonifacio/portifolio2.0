12aa20452f1baf4c9ce9f7f18bb258913ce43654
João Bonifácio <joaojunior.b22@gmail.com>
2023-05-15 15:58:36 -0300

    projects-page-fixed

--- pages/projetos/index.tsx
+++ pages/projetos/index.tsx
@@ -5,11 +5,12 @@ import projects from "@data/projects.json"
 export default function Projects(){
     return (
         <>
+            {alert("Você pode clicar nos projetos para exibir mais detalhes")}
             <h1 className="text-center text-xl text-gray-300 bg-gray-600 p-2"> Projetos </h1>
             <div className="p-8 flex flex-wrap justify-center bg-gray-400">
                 {projects.map(e => (
                     <Link href={"/projetos/"+e.id} className="flex flex-wrap mx-5 bg-gray-300 hover:bg-gray-400 rounded-md mb-8" key={e.id}>
-                    <Image src={e.image} width="160" height="110" alt="..." className="justify-center mr-2 rounded-l-md"/>
+                    <Image src={e.image} width="160" height="110" alt="..." className="rounded-l-md mx-auto p-2"/>
                         <div className="max-w-md p-4">
                             <h4 className="font-bold">{e.title}</h4>
                             <p>{e.short_desc}</p>
