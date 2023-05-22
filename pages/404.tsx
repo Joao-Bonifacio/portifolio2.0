4e1754cae6ba13858e5f1dfd54a9a917d4d667af
João Bonifácio <joaojunior.b22@gmail.com>
2023-05-22 08:17:50 -0300

    404-page-fix

--- pages/404.tsx
+++ pages/404.tsx
@@ -4,7 +4,7 @@ export default function NotFound(){
     return(
         <div className="mx-auto my-auto p-5 text-center pt-[20%]">
             <h1 className="text-3xl"><span className="text-red-400">404</span> - Página não encontrada...</h1>
-            <Link href='/' className="text-blue-500 hover:text-blue-700 text-xl">go back home</Link>
+            <Link href='/' className="text-blue-500 hover:text-blue-700 text-xl">🔙Página inicial</Link>
         </div>
     )
 }
