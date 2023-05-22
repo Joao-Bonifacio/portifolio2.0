9feb3c143ea50dc4b8dfbf8189da57db63660c6c
João Bonifácio <joaojunior.b22@gmail.com>
2023-05-22 08:16:20 -0300

    404-page-fix

--- pages/404.tsx
+++ pages/404.tsx
@@ -2,9 +2,9 @@ import Link from "next/link";
 
 export default function NotFound(){
     return(
-        <>
-        <h1>404 - Page not found</h1>
-        <Link href='/'>go back home</Link>
-        </>
+        <div className="mx-auto my-auto p-5 text-center pt-[20%]">
+            <h1 className="text-3xl"><span className="text-red-400">404</span> - Página não encontrada...</h1>
+            <Link href='/' className="text-blue-500 hover:text-blue-700 text-xl">go back home</Link>
+        </div>
     )
 }
